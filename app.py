c7400eccd2c53acfe599f8234a9124e015cf4c90
Fraol Gelana <fraolgela@gmail.com>
2023-06-09 12:47:10 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,160 @@
+import threading
+import cv2
+import numpy as np
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+import torch
+import torchvision.transforms as transforms
+from infer import infer
+
+
+class ImageApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("MedSAM App")
+        self.canvas_width = 256
+        self.canvas_height = 256
+
+        # Create canvas
+        self.canvas = tk.Canvas(
+            root, width=self.canvas_width, height=self.canvas_height
+        )
+        self.canvas.pack()
+
+        # Bind mouse events
+        self.canvas.bind("<ButtonPress-1>", self.on_mouse_press)
+        self.canvas.bind("<B1-Motion>", self.on_mouse_drag)
+        self.canvas.bind("<ButtonRelease-1>", self.on_mouse_release)
+
+        # Initialize variables
+        self.start_x = None
+        self.start_y = None
+        self.rect = None
+        self.image = None
+        self.tk_image = None
+
+        # Create button for picking image
+        self.pick_image_button = tk.Button(
+            root, text="Pick Image", command=self.pick_image
+        )
+        self.pick_image_button.pack()
+
+    def pick_image(self):
+        # Open a file dialog to choose an image file
+        file_path = filedialog.askopenfilename(
+            initialdir="/mnt/dysk_roboczy/fgela/Dataset_BUSI_with_GT",
+            title="Select Image",
+            filetypes=[("Image Files", "*.png *.jpg *.jpeg")],
+        )
+
+        if file_path:
+            # Load the selected image
+            self.load_image(file_path)
+
+    def load_image(self, image_path):
+        # Open the image file
+        image = Image.open(image_path)
+
+        image = image.resize((256, 256))
+
+        self.image = image
+        # Resize the image to fit the canvas
+        if image.width > self.canvas_width or image.height > self.canvas_height:
+            image.thumbnail((self.canvas_width, self.canvas_height), Image.ANTIALIAS)
+
+        # Create Tkinter-compatible image object
+        self.tk_image = ImageTk.PhotoImage(image)
+
+        # Clear canvas and display the image
+        self.canvas.delete("all")
+        self.canvas.create_image(0, 0, image=self.tk_image, anchor="nw")
+
+    def on_mouse_press(self, event):
+        # Store the starting position of the bounding box
+        self.start_x = event.x
+        self.start_y = event.y
+
+    def on_mouse_drag(self, event):
+        # Update the bounding box as the mouse is dragged
+        if self.rect:
+            self.canvas.delete(self.rect)
+        self.rect = self.canvas.create_rectangle(
+            self.start_x, self.start_y, event.x, event.y, outline="red"
+        )
+
+    def on_mouse_release(self, event):
+        # Print the coordinates of the bounding box
+        print("Bounding box coordinates:")
+        print(f"Top-left: ({self.start_x}, {self.start_y})")
+        print(f"Bottom-right: ({event.x}, {event.y})")
+
+        # to tensor
+        transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                # transforms.Resize((1024, 1024)),
+            ]
+        )
+        tensor_img = transform(self.image)
+
+        bbox_tensor = torch.tensor([self.start_x, self.start_y, event.x, event.y])
+
+        # Start a new thread for inference
+        threading.Thread(
+            target=self.run_inference, args=(tensor_img, bbox_tensor)
+        ).start()
+
+    def run_inference(self, tensor_img, bounding_box):
+        pred_mask = infer(tensor_img.unsqueeze(0), bounding_box)
+
+        print(f"white pix count = {(pred_mask == 255).sum()}")
+
+        pred_mask = pred_mask.squeeze(0).cpu()
+
+        print(f"pred shape = {pred_mask.shape}")
+
+        self.update_gui(pred_mask, tensor_img)
+
+    def update_gui(self, pred_mask, tensor_img):
+        # Convert the tensor mask to a PIL image
+        print(f"{pred_mask.shape}")
+        color_mask = np.zeros((256, 256, 3), dtype=np.uint8)
+        color_mask[:, :] = (251, 10, 0)
+
+        color_mask = cv2.bitwise_and(
+            color_mask,
+            color_mask,
+            mask=np.uint8(pred_mask.squeeze(0).numpy()),
+        )
+
+        image_cv = np.uint8(
+            tensor_img.permute(1, 2, 0).cpu().numpy() * 255
+        )  # np.array(self.image)
+
+        # color_mask = cv2.resize(color_mask, (self.image.width, self.image.height))
+
+        print(f"{color_mask.shape=}")
+
+        image_cv = cv2.addWeighted(color_mask, 0.3, image_cv, 1, 0)
+
+        # Display the overlay image on the canvas
+        image_pil = Image.fromarray(
+            image_cv
+        )  # cv2.cvtColor(image_cv, cv2.COLOR_BGR2RGB))
+
+        image_tk = ImageTk.PhotoImage(image_pil)
+
+        self.canvas.delete("all")
+        self.canvas.create_image(
+            0, 0, image=image_tk, anchor="nw"
+        )  # tk_overlay, anchor="nw")
+
+        # Store a reference to the PhotoImage object to prevent it from being garbage collected
+        self.canvas.image = image_tk
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ImageApp(root)
+    root.mainloop()
---
+++ infer.py
@@ -0,0 +1,66 @@
+import cv2
+import torch
+import numpy as np
+from segment_anything import sam_model_registry, SamPredictor
+import torchvision.transforms.functional as TF
+from segment_anything.utils.transforms import ResizeLongestSide
+from dataclasses import dataclass
+
+
+@dataclass()
+class Config:
+    sam_checkpoint_dir: str = (
+        "/mnt/dysk_roboczy/fgela/MedSAM_GUI/finetune_weights/sam_vit_b_01ec64.pth"
+    )
+    med_sam_checkpoint_dir: str = "/mnt/dysk_roboczy/fgela/MedSAM_GUI/finetune_weights/medsam_finetune_2023-05-16-09-55.pth"
+
+    model_type: str = "vit_b"
+    device: str = "cuda:0"
+
+
+@torch.inference_mode()
+def infer(image: torch.Tensor, bbox: tuple):
+    config = Config()
+    # initialize sam model
+
+    image = (image - image.min()) / (image.max() - image.min()) * 255.0
+
+    H, W = image.shape[2], image.shape[3]
+
+    image = image.to(config.device)
+
+    sam_model = sam_model_registry[config.model_type](
+        checkpoint=config.med_sam_checkpoint_dir
+    ).to(config.device)
+
+    sam_trans = ResizeLongestSide(sam_model.image_encoder.img_size)
+    # trans_image = image.unsqueeze(0).to(device)
+    trans_image = TF.resize(image, (1024, 1024))
+
+    box = sam_trans.apply_boxes(bbox, (256, 256))
+
+    box_tensor = torch.as_tensor(box, dtype=torch.float, device=config.device)
+    # Get predictioin mask
+
+    image_embeddings = sam_model.image_encoder(trans_image)  # (B,256,64,64)
+
+    sparse_embeddings, dense_embeddings = sam_model.prompt_encoder(
+        points=None,
+        boxes=box_tensor,
+        masks=None,
+    )
+    mask_predictions, _ = sam_model.mask_decoder(
+        image_embeddings=image_embeddings.to(config.device),  # (B, 256, 64, 64)
+        image_pe=sam_model.prompt_encoder.get_dense_pe(),  # (1, 256, 64, 64)
+        sparse_prompt_embeddings=sparse_embeddings,  # (B, 2, 256)
+        dense_prompt_embeddings=dense_embeddings,  # (B, 256, 64, 64)
+        multimask_output=False,
+    )
+    mask_predictions = torch.sigmoid(mask_predictions)
+
+    mask_predictions = (mask_predictions > 0.5).int() * 255
+    torch.cuda.empty_cache()
+
+    del sam_model
+
+    return mask_predictions
